40d9f02bf831e104aafb7a4d37b216e3fda20ee5
LucyKorotkova <lusi5000@icloud.com>
2025-03-19 22:09:38 +0300

    Update createAdder.test.js

--- Задание 11.6.4 (HW-01)/createAdder.test.js
+++ Задание 11.6.4 (HW-01)/createAdder.test.js
@@ -13,13 +13,21 @@ test('Сложение нулей', () => {
 });
 
 test('Передача строки вместо числа', () => {
-    expect(createAdder("5")(3)).toBeNaN();
+    expect(createAdder("5")(3)).toBe("53"); // Строка, а не NaN
 });
 
 test('Передача null как второго аргумента', () => {
-    expect(createAdder(5)(null)).toBeNaN();
+    expect(createAdder(5)(null)).toBe(5); // null приводится к 0
 });
 
 test('Передача NaN в качестве первого аргумента', () => {
-    expect(createAdder(NaN)(2)).toBeNaN();
+    expect(createAdder(NaN)(2)).toBe(NaN);
+});
+
+test('Передача строки как второго аргумента', () => {
+        expect(createAdder(5)("3")).toBe("53");
+});
+
+test('Передача NaN как второго аргумента', () => {
+        expect(createAdder(5)(NaN)).toBe(NaN)
 });
